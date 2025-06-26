f2457b721730ceae7718d8e4f72f4624d1043462
Elijah-ug <elijahtalentmugisha14@gmail.com>
2025-06-26 13:46:47 +0300

    modified UI

--- client/src/components/RiderDashboard.jsx
+++ client/src/components/RiderDashboard.jsx
@@ -26,7 +26,7 @@ const RiderDashboard = ({ riderData, assignedTrips, onWithdraw }) => {
         })
   }, [])
   const showTripInfo = riderProfile?.user?.toLowerCase() === tripInfo?.rider?.toLowerCase()
-  console.log("showTripInfo: " + showTripInfo)
+  console.log("Number(tripInfo.tripId) : " + typeof(Number(tripInfo.tripId)) )
   console.log("tripInfo.rider: ", tripInfo.rider)
   return (
     <div className="bg-gray-700 min-h-screen text-white">
@@ -37,17 +37,27 @@ const RiderDashboard = ({ riderData, assignedTrips, onWithdraw }) => {
         <div className="p-4 rounded-xl shadow flex items-center justify-around gap-6 text-amber-400 bg-gray-800">
           <div>
             <h2 className="text-xl font-semibold mb-2 text-white">🏍️ Rider Info</h2>
-            <p><strong>Address:</strong> {riderProfile?.user?.slice(0, 7)}...{riderProfile?.user?.slice(-5)}</p>
-            <p><strong>Rider ID:</strong> {riderProfile?.riderId}</p>
+            <p><strong>Address:</strong>
+              <span className="ml-2">{riderProfile?.user?.slice(0, 7)}...{riderProfile?.user?.slice(-5)}</span>
+            </p>
+            <p><strong>Rider ID:</strong>
+              <span className="ml-2">{riderProfile?.riderId? (Number(riderProfile?.riderId) + 1) : null}</span>
+            </p>
           </div>
           <div>
             <h2 className="text-xl font-semibold mb-2 text-white">⭐ Stats</h2>
-            <p><strong>Stars:</strong> {riderProfile?.stars}</p>
-            <p><strong>Total Trips:</strong> {riderProfile?.totalTrips}</p>
+            <p><strong>Stars:</strong>
+              <span className="ml-2">{riderProfile?.stars}</span>
+            </p>
+            <p><strong>Total Trips:</strong>
+              <span className="ml-2">{riderProfile?.totalTrips}</span>
+            </p>
           </div>
           <div>
             <h2 className="text-xl font-semibold mb-2 text-white">💰 Earnings</h2>
-            <p><strong>Available:</strong> {riderProfile?.earnings} ETH</p>
+            <p><strong>Available:</strong>
+              <span className="ml-2">{riderProfile?.earnings} ETH</span>
+            </p>
           </div>
         </div>
 
@@ -79,7 +89,7 @@ const RiderDashboard = ({ riderData, assignedTrips, onWithdraw }) => {
           ) : (
               <div className="border-b py-2 flex justify-between items-center">
                 <div>
-                  <p>Trip #{tripInfo.tripId}</p>
+                  <p>Trip #{tripInfo?.tripId? (Number(tripInfo.tripId) + 1) : null}</p>
                   <p>Rider: {tripInfo.rider}</p>
                   <p>Client: {tripInfo.client}</p>
                   <p>Fare: {tripInfo.fare} ETH</p>
